d7248191215e0735840fe2e9b9018c665990bafb
liao657 <w5862591@gmail.com>
2021-01-25 23:03:13 +0800

    改

---
+++ main.js
@@ -0,0 +1,19 @@
+$("#banner").slick({
+    dots: true,
+    infinite: true,
+    speed: 1000,
+    fade: true,
+    autoplay:true,
+    autoplayspeed:1.5,
+});
+$("#service").slick({
+    speed:500,
+    autoplay:true,
+    autoplayspeed:1.5,
+    slidesToShow:3,
+})
+$('.carousel').carousel({
+    interval: 2000,
+    autoplay:true,
+    autoplayspeed:1.5,
+  })
